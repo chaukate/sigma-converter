0d0a51360ab4a465d95489a2519ccf0e125748dd
Santosh Shrestha <santosh.chaukate@hotmail.com>
2021-02-17 23:29:55 +0545

    master - image converter and image resize testing completed on implementation.

--- Sigma.Converter/Image/ImageConverter.cs
+++ Sigma.Converter/Image/ImageConverter.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
-using Sigma.Converter.Enumerations;
 using System;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -15,10 +14,7 @@ namespace Sigma.Converter
     public static class ImageConverter
     {
         private static readonly string[] ContentTypes = { "image/apng", "image/avif", "image/bmp", "image/gif", "image/jfif", "image/jpeg", "image/png", "image/svg+xml", "image/tiff", "image/webp" };
-        private static double _height;
-        private static double _width;
-        private static int _quality = 75;
-        private static Image _image;
+        private static int _quality = 99;
 
         /// <summary>
         /// Validates whether file content type is image type.
@@ -29,13 +25,7 @@ namespace Sigma.Converter
         {
             if (formFile.Length > 0)
             {
-                foreach (var contentType in ContentTypes)
-                {
-                    if (formFile.ContentType.ToLower() == contentType)
-                    {
-                        return true;
-                    }
-                }
+                return ContentTypes.Contains(formFile.ContentType.ToLower());
             }
             return false;
         }
@@ -49,12 +39,27 @@ namespace Sigma.Converter
         {
             if (formFile.IsValidImage())
             {
-                using var stream = new MemoryStream();
+                var stream = new MemoryStream();
                 formFile.CopyTo(stream);
                 var image = Image.FromStream(stream);
+                stream.Flush();
                 return image;
             }
             throw new Exception("Invalid image file.");
+        }
+
+        /// <summary>
+        /// Converts base64 string to image.
+        /// </summary>
+        /// <param name="base64"></param>
+        /// <returns></returns>
+        public static Image ToImage(this string base64)
+        {
+            var bytes = Convert.FromBase64String(base64);
+            var stream = new MemoryStream(bytes, 0, bytes.Length);
+            var image = Image.FromStream(stream);
+            stream.Flush();
+            return image;
         }
 
         /// <summary>
@@ -64,7 +69,7 @@ namespace Sigma.Converter
         /// <param name="name"></param>
         /// <param name="extension"></param>
         /// <returns></returns>
-        public static IFormFile ToFormFile(this string base64, string name = "filename", string extension = ".png")
+        public static IFormFile ToFormFile(this string base64, string name = "filename", string extension = ".jpeg")
         {
             var bytes = Convert.FromBase64String(base64);
             var stream = new MemoryStream(bytes, 0, bytes.Length);
@@ -90,13 +95,11 @@ namespace Sigma.Converter
         {
             if (formFile.IsValidImage())
             {
-                string base64 = "";
-                using (var stream = new MemoryStream())
-                {
-                    formFile.CopyTo(stream);
-                    var fileBytes = stream.ToArray();
-                    base64 = Convert.ToBase64String(fileBytes);
-                }
+                var stream = new MemoryStream();
+                formFile.CopyTo(stream);
+                var fileBytes = stream.ToArray();
+                string base64 = Convert.ToBase64String(fileBytes);
+                stream.Flush();
                 return base64;
             }
             throw new Exception("Invalid image file.");
@@ -109,7 +112,6 @@ namespace Sigma.Converter
         /// <returns></returns>
         public static string ToBase64String(this Image image)
         {
-            string base64 = "";
             var stream = new MemoryStream();
 
             var qualityParamId = Encoder.Quality;
@@ -119,176 +121,9 @@ namespace Sigma.Converter
             image.Save(stream, codec, encoderParameters);
 
             var imageBytes = stream.ToArray();
-            base64 = Convert.ToBase64String(imageBytes);
+            var base64 = Convert.ToBase64String(imageBytes);
+            stream.Flush();
             return base64;
-        }
-
-        /// <summary>
-        /// Sets size
-        /// </summary>
-        /// <param name="size"></param>
-        private static void SetSize(ImageSizes size)
-        {
-            switch (size)
-            {
-                case ImageSizes.x32:
-                    _height = 32;
-                    _width = 32;
-                    break;
-                case ImageSizes.x64:
-                    _height = 64;
-                    _width = 64;
-                    break;
-                case ImageSizes.x128:
-                    _height = 128;
-                    _width = 128;
-                    break;
-                case ImageSizes.x150:
-                    _height = 150;
-                    _width = 150;
-                    break;
-                case ImageSizes.x256:
-                    _height = 256;
-                    _width = 256;
-                    break;
-                case ImageSizes.x800:
-                    _height = 800;
-                    _width = 800;
-                    break;
-                default:
-                    throw new Exception("Invalid size.");
-            }
-        }
-
-        /// <summary>
-        /// Resize image to defined size type and return image. x32 is height and weight = 32.
-        /// </summary>
-        /// <param name="image"></param>
-        /// <param name="size"></param>
-        /// <returns></returns>
-        public static Image ResizeToImage(this Image image, ImageSizes size)
-        {
-            SetSize(size);
-            _image = image;
-            SetHeightWidth(_image);
-            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
-            return resizedImage;
-        }
-
-        /// <summary>
-        /// Resize image to defined size type and return as base64 string. x32 is height and weight = 32.
-        /// </summary>
-        /// <param name="image"></param>
-        /// <param name="size"></param>
-        /// <returns></returns>
-        public static string ResizeToBase64(this Image image, ImageSizes size)
-        {
-            SetSize(size);
-            _image = image;
-            SetHeightWidth(_image);
-            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
-            var base64 = resizedImage.ToBase64String();
-            return base64;
-        }
-
-        /// <summary>
-        /// Resize to defined size type and return image. x32 is height and weight = 32.
-        /// </summary>
-        /// <param name="formFile"></param>
-        /// <param name="size"></param>
-        /// <returns></returns>
-        public static Image ResizeToImage(this IFormFile formFile, ImageSizes size)
-        {
-            SetSize(size);
-            _image = formFile.ToImage();
-            SetHeightWidth(_image);
-            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
-            return resizedImage;
-        }
-
-        /// <summary>
-        /// Resize to defined size type and return base64 string. x32 is height and weight = 32.
-        /// </summary>
-        /// <param name="formFile"></param>
-        /// <param name="size"></param>
-        /// <returns></returns>
-        public static string ResizeToBase64(this IFormFile formFile, ImageSizes size)
-        {
-            SetSize(size);
-            _image = formFile.ToImage();
-            SetHeightWidth(_image);
-            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
-            var base64 = resizedImage.ToBase64String();
-            return base64;
-        }
-
-        /// <summary>
-        /// Resize to defined size type and return image. x32 is height and weight = 32.
-        /// </summary>
-        /// <param name="base64"></param>
-        /// <param name="size"></param>
-        /// <returns></returns>
-        public static Image ResizeToImage(this string base64, ImageSizes size)
-        {
-            var bytes = Convert.FromBase64String(base64);
-            var stream = new MemoryStream(bytes, 0, bytes.Length);
-            SetSize(size);
-            _image = Image.FromStream(stream);
-            SetHeightWidth(_image);
-            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
-            return resizedImage;
-        }
-
-        /// <summary>
-        /// Resize to defined size type and return base64 string. x32 is height and weight = 32.
-        /// </summary>
-        /// <param name="base64"></param>
-        /// <param name="size"></param>
-        /// <returns></returns>
-        public static string ResizeToBase64(this string base64, ImageSizes size)
-        {
-            var bytes = Convert.FromBase64String(base64);
-            var stream = new MemoryStream(bytes, 0, bytes.Length);
-            SetSize(size);
-            _image = Image.FromStream(stream);
-            SetHeightWidth(_image);
-            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
-            var str = resizedImage.ToBase64String();
-            return str;
-        }
-
-        /// <summary>
-        /// Sets height and width.
-        /// </summary>
-        /// <param name="image"></param>
-        private static void SetHeightWidth(Image image)
-        {
-            double ratio;
-            if (image.Height <= _height && image.Width <= _width)
-            {
-                _height = image.Height;
-                _width = image.Width;
-            }
-            else if (image.Height > image.Width)
-            {
-                if (image.Height <= _height)
-                {
-                    _height = image.Height;
-                    _width = _height;
-                }
-                ratio = (double)image.Width / (double)image.Height;
-                _width *= ratio;
-            }
-            else if (image.Width >= image.Height)
-            {
-                if (image.Width <= _width)
-                {
-                    _width = image.Width;
-                    _height = _width;
-                }
-                ratio = (double)image.Height / (double)image.Width;
-                _height *= ratio;
-            }
         }
     }
 }
---
+++ Sigma.Converter/Image/ImageResize.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.AspNetCore.Http;
+using Sigma.Converter.Enumerations;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace Sigma.Converter
+{
+    /// <summary>
+    /// Imge Resize
+    /// </summary>
+    public static class ImageResize
+    {
+        private static double _height;
+        private static double _width;
+        private static Image _image;
+
+        /// <summary>
+        /// Resize image to defined size type and return image. x32 is height and weight = 32.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static Image ResizeToImage(this Image image, ImageSizes size)
+        {
+            SetSize(size);
+            _image = image;
+            SetHeightWidth(_image);
+            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
+            return resizedImage;
+        }
+
+        /// <summary>
+        /// Resize image to defined size type and return as base64 string. x32 is height and weight = 32.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static string ResizeToBase64(this Image image, ImageSizes size)
+        {
+            SetSize(size);
+            _image = image;
+            SetHeightWidth(_image);
+            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
+            var base64 = resizedImage.ToBase64String();
+            return base64;
+        }
+
+        /// <summary>
+        /// Resize to defined size type and return image. x32 is height and weight = 32.
+        /// </summary>
+        /// <param name="formFile"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static Image ResizeToImage(this IFormFile formFile, ImageSizes size)
+        {
+            SetSize(size);
+            _image = formFile.ToImage();
+            SetHeightWidth(_image);
+            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
+            return resizedImage;
+        }
+
+        /// <summary>
+        /// Resize to defined size type and return base64 string. x32 is height and weight = 32.
+        /// </summary>
+        /// <param name="formFile"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static string ResizeToBase64(this IFormFile formFile, ImageSizes size)
+        {
+            SetSize(size);
+            _image = formFile.ToImage();
+            SetHeightWidth(_image);
+            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
+            var base64 = resizedImage.ToBase64String();
+            return base64;
+        }
+
+        /// <summary>
+        /// Resize to defined size type and return image. x32 is height and weight = 32.
+        /// </summary>
+        /// <param name="base64"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static Image ResizeToImage(this string base64, ImageSizes size)
+        {
+            var bytes = Convert.FromBase64String(base64);
+            var stream = new MemoryStream(bytes, 0, bytes.Length);
+            SetSize(size);
+            _image = Image.FromStream(stream);
+            stream.Flush();
+            SetHeightWidth(_image);
+            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
+            return resizedImage;
+        }
+
+        /// <summary>
+        /// Resize to defined size type and return base64 string. x32 is height and weight = 32.
+        /// </summary>
+        /// <param name="base64"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static string ResizeToBase64(this string base64, ImageSizes size)
+        {
+            var bytes = Convert.FromBase64String(base64);
+            var stream = new MemoryStream(bytes, 0, bytes.Length);
+            SetSize(size);
+            _image = Image.FromStream(stream);
+            stream.Flush();
+            SetHeightWidth(_image);
+            var resizedImage = _image.GetThumbnailImage((int)_width, (int)_height, () => false, IntPtr.Zero);
+            var str = resizedImage.ToBase64String();
+            return str;
+        }
+
+        #region Private Methods
+        /// <summary>
+        /// Sets size
+        /// </summary>
+        /// <param name="size"></param>
+        private static void SetSize(ImageSizes size)
+        {
+            switch (size)
+            {
+                case ImageSizes.x32:
+                    _height = 32;
+                    _width = 32;
+                    break;
+                case ImageSizes.x64:
+                    _height = 64;
+                    _width = 64;
+                    break;
+                case ImageSizes.x128:
+                    _height = 128;
+                    _width = 128;
+                    break;
+                case ImageSizes.x150:
+                    _height = 150;
+                    _width = 150;
+                    break;
+                case ImageSizes.x256:
+                    _height = 256;
+                    _width = 256;
+                    break;
+                case ImageSizes.x800:
+                    _height = 800;
+                    _width = 800;
+                    break;
+                default:
+                    throw new Exception("Invalid size.");
+            }
+        }
+
+        /// <summary>
+        /// Sets height and width.
+        /// </summary>
+        /// <param name="image"></param>
+        private static void SetHeightWidth(Image image)
+        {
+            double ratio;
+            if (image.Height <= _height && image.Width <= _width)
+            {
+                _height = image.Height;
+                _width = image.Width;
+            }
+            else if (image.Height > image.Width)
+            {
+                if (image.Height <= _height)
+                {
+                    _height = image.Height;
+                    _width = _height;
+                }
+                ratio = (double)image.Width / (double)image.Height;
+                _width *= ratio;
+            }
+            else if (image.Width >= image.Height)
+            {
+                if (image.Width <= _width)
+                {
+                    _width = image.Width;
+                    _height = _width;
+                }
+                ratio = (double)image.Height / (double)image.Width;
+                _height *= ratio;
+            }
+        }
+        #endregion
+    }
+}
